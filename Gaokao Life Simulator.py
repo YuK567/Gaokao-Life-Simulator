8164c4904c9b6f0f2d15f53ca2c94ccf3cd50d12
Kaiser <152428456+YuK567@users.noreply.github.com>
2025-10-13 23:10:12 +0000

    commit

--- Gaokao Life Simulator.py
+++ Gaokao Life Simulator.py
@@ -1,2 +1,12 @@
-import sys
-import time
+import sys, time,json,os,random
+from colorama import Fore, Back, Style, init
+init(autoreset=True)   
+
+SAVE_FILE = "gaokao_save.json"
+
+def slow_print(text, delay=0.02, color=Fore.WHITE):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print(Style.RESET_ALL)
